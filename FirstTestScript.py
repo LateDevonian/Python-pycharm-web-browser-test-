374bfd5c3946b8024da64d0c89d210cedc614a6c
LateDevonian <agean27@gmail.com>
2019-04-02 09:04:17 +1100

    intital commit test script in python

---
+++ FirstTestScript.py
@@ -0,0 +1,3 @@
+from selenium import webdriver
+driver = webdriver.Chrome("/home/avril/PycharmProjects/SeleniumSampleProject/drivers/chromedriver")
+driver.get("http:shiny.myob.com/")
---
+++ README.md
@@ -0,0 +1,4 @@
+Use pycharm to run requires python 3.6
+drivers: geckodriver, chromedriver
+using Katalan recorder and modified
+Created at workshop for MYOB
